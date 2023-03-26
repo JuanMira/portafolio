626ae476961bc2eccd533cadea97324d8d42d62c
JuanMira <juanmira720@gmail.com>
2023-03-26 02:47:53 -0500

    Bug Interfaz iservice, campo no existente

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -2,14 +2,13 @@ import { FunctionComponent } from "react";
 import { IServices } from "../type";
 
 const ServiceCard: FunctionComponent<{ service: IServices }> = ({
-  service: { Icon, about, title },
+  service: { Icon,  title },
 }) => {
   return (
     <div className="flex items-center p-2 space-x-4">
       <Icon className="w-12 h-12 text-green" />
       <div>
-        <h4 className="font-bold">{title}</h4>
-        <p>{about}</p>
+        <h4 className="font-bold">{title}</h4>        
       </div>
     </div>
   );
